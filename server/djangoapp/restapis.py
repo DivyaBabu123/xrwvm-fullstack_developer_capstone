c38ee38ec67e9c03b11e4a9b47f3c95cc83897a8
DivyaBabu123 <divyaoct94@gmail.com>
2026-04-20 11:09:59 -0400

    fix lint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,11 +14,11 @@ sentiment_analyzer_url = os.getenv(
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
-def get_request (endpoint, **kwargs):
+def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params= params + key + "=" + value + "&"
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
@@ -49,9 +49,9 @@ def analyze_review_sentiments(text):
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except Exception as e:
