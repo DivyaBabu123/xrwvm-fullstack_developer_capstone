281962a40cd1987ceff65428faf24835fa927a8c
DivyaBabu123 <divyaoct94@gmail.com>
2026-04-21 03:12:56 -0400

    linting fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,12 +93,10 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append(
-            {
+        cars.append({
             "CarModel": car_model.name,
             "CarMake": car_model.car_make.name
-            }
-        )
+        })
     return JsonResponse({"CarModels": cars})
 
 
